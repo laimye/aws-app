766e32d4529916aa43ddcb1093061db1331089d3
Tim Davis <contact@timdav.is>
2022-06-19 16:01:18 -0700

    make region var consistent

--- server/uploads-middleware.js
+++ server/uploads-middleware.js
@@ -5,7 +5,7 @@ import multerS3 from 'multer-s3'
 import { S3Client } from '@aws-sdk/client-s3'
 
 const s3 = new S3Client({
-  region: process.env.AWS_S3_REGION,
+  region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
