c41704eb20ecdcb75a4efe76ec46a0fb556fa5c4
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-06-19 15:12:29 -0700

    add image upload post

--- client/app.jsx
+++ client/app.jsx
@@ -5,20 +5,43 @@ import { Box, Button, useToast, useDisclosure } from '@chakra-ui/react'
 import ImageUpload from './components/image-upload'
 import ImageGallery from './components/image-gallery'
 
-const fallbackData = Array(6).fill()
+const fallbackData = Array(12).fill()
 
-const getImages = url => axios.get(url).then(({ data }) => data)
+const minDelay = (delay, operation) => (...args) => Promise.all([
+  operation(...args),
+  new Promise(resolve => setTimeout(resolve, delay))
+]).then(([result]) => result)
+
+const getImages = minDelay(1500, url => axios.get(url).then(({ data }) => data))
 
 export default function App() {
 
   const toast = useToast()
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const { data, error } = useSWR('/api/uploads', getImages, {
+  const { data, error, mutate } = useSWR('/api/uploads', getImages, {
     fallbackData
   })
 
+  const addImage = async formData => {
+    try {
+      const { data: newImage } = await axios.post('/api/uploads', formData)
+      mutate([...data, newImage])
+    } catch (err) {
+      console.error(err)
+      toast({
+        title: 'Uh-oh!',
+        description: 'There was a problem uploading your image!',
+        status: 'error',
+        position: 'bottom-left'
+      })
+    } finally {
+      onClose()
+    }
+  }
+
   useEffect(() => {
     if (typeof error !== 'undefined') {
+      console.error(error)
       toast({
         title: 'Uh-oh!',
         description: 'There was a problem loading the images...',
@@ -31,7 +54,7 @@ export default function App() {
   return (
     <>
       <ImageGallery images={data} />
-      <ImageUpload isOpen={isOpen} onClose={onClose} />
+      <ImageUpload isOpen={isOpen} onClose={onClose} addImage={addImage} />
       <Box display="flex" justifyContent="stretch" padding={6}>
         <Button colorScheme="teal" onClick={onOpen} flex={1}>
           Add an Image
--- client/components/image-upload.jsx
+++ client/components/image-upload.jsx
@@ -1,26 +1,97 @@
+import { useState, useEffect, useRef } from 'react'
 import {
-  Box, Button,
+  Box, Image, Button, VisuallyHidden,
   FormControl, FormLabel, Input, FormHelperText,
   Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton
 } from '@chakra-ui/react'
 
-export default function ImageUpload({ isOpen, onClose }) {
+const isValidFileType = filename => (
+  ['.jpeg', '.jpg', '.png', '.webp', '.gif'].some(ext => filename.endsWith(ext))
+)
+
+export default function ImageUpload({ isOpen, onClose, addImage }) {
+
+  const fileInputRef = useRef(null)
+  const [file, setFile] = useState(null)
+  const [caption, setCaption] = useState('')
+  const [preview, setPreview] = useState(null)
+
+  const handleCaptionChange = event => {
+    setCaption(event.target.value)
+  }
+
+  const handleFileChange = event => {
+    setFile(event.target.files[0])
+  }
+
+  const handleImageClick = event => {
+    fileInputRef.current?.click()
+  }
+
+  const handleDragOver = event => {
+    event.preventDefault()
+  }
+
+  const handleDrop = event => {
+    event.preventDefault()
+    const file = event.dataTransfer.files?.[0]
+    const filename = file?.name.toLowerCase() ?? ''
+    if (!isValidFileType(filename)) return
+    setFile(file)
+  }
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+    if (file === null || caption.trim() === '') return
+    const formData = new FormData()
+    formData.append('image', file)
+    formData.append('caption', caption)
+    await addImage(formData)
+  }
+
+  useEffect(() => {
+    return () => URL.revokeObjectURL(preview)
+  }, [preview])
+
+  useEffect(() => {
+    if (file !== null) setPreview(URL.createObjectURL(file))
+  }, [file])
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Upload Image</ModalHeader>
         <ModalCloseButton onClick={onClose} />
+        <Box
+          height={200}
+          onDrop={handleDrop}
+          onClick={handleImageClick}
+          onDragOver={handleDragOver}>
+          { preview !== null &&
+            <Image
+              fit="contain" height={200}
+              width="100%" src={preview} alt={caption} />
+          }
+        </Box>
         <Box padding={6}>
-          <form>
+          <form onSubmit={handleSubmit}>
+            <VisuallyHidden>
+              <input
+                type="file"
+                name="image"
+                ref={fileInputRef}
+                onChange={handleFileChange}
+                accept=".jpg, .jpeg, .png, .webp,.gif" />
+            </VisuallyHidden>
             <FormControl isRequired>
               <FormLabel htmlFor="caption">Image Caption</FormLabel>
-              <Input id="caption" type="text" />
+              <Input id="caption" type="text" onChange={handleCaptionChange} />
               <FormHelperText>What is this a picture of?</FormHelperText>
             </FormControl>
             <Box mt={6} display="flex" justifyContent="flex-end">
               <Button type="submit" variant="solid" colorScheme="teal">
-                Upload
+                Add
               </Button>
             </Box>
           </form>
